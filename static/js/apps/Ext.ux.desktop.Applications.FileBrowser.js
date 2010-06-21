dab4a71e8d617ff1ed3f9383400aeacfcd7f9cfe
Gary van Woerkens <gary@chewam.com>
2010-06-21 19:08:41 +0200

    start FileBrowser application

---
+++ static/js/apps/Ext.ux.desktop.Applications.FileBrowser.js
@@ -0,0 +1,50 @@
+Ext.ns('Ext.ux.desktop.Applications');
+
+Ext.ux.desktop.Applications.FileBrowser = Ext.extend(Ext.ux.desktop.Applications.Application, {
+
+    name:"File Browser"
+
+    ,iconCls16:"icon-filebrowser-16"
+
+    ,iconCls64:"icon-filebrowser-64"
+
+    ,constructor:function() {
+
+        this.menu = new Ext.menu.Menu({
+            items:[{
+                text:"FileBrowser 1"
+            }]
+        });
+
+        Ext.ux.desktop.Applications.FileBrowser.superclass.constructor.call(this);
+    }
+
+    ,start:function() {
+
+        this.filebrowser = new Ext.ux.FileBrowser({
+            root:"pof"
+            ,rootText:"Home"
+            ,url:""
+            ,enableBrowser:false
+            ,enableUpload:false
+            ,statusBar:false
+            ,readOnly:true
+        });
+
+        new Ext.ux.desktop.Window({
+            width:600
+            ,height:300
+            ,layout:"fit"
+            ,border:false
+            ,title:this.name
+            ,iconCls:this.iconCls16
+            ,items:this.filebrowser
+        }).show();
+
+    }
+
+});
+
+Ext.ux.desktop.Desktop.registerApplication(Ext.ux.desktop.Applications.FileBrowser);
+
+
